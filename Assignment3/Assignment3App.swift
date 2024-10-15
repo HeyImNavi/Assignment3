bd3dccf6aef13a532065dbcbaa827a112788be54
Aaron Arreola <legendofnavi1090@gmail.com>
2024-10-15 15:28:21 -0500

    Initial Commit

---
+++ Assignment3/Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3App.swift
+//  Assignment3
+//
+//  Created by Maggie Thomas on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
