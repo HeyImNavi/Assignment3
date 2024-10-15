d17d2e4e6d92f303fd4bad20ecc25928e184e044
Aaron Arreola <legendofnavi1090@gmail.com>
2024-10-15 15:36:32 -0500

    Added tab onto ControlView

--- Assignment3/ContentView.swift
+++ Assignment3/ContentView.swift
@@ -11,10 +11,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        Text("Hello")
-        Text("second")
-        
+        //Call TabControlView
+        TabControlView()
     }//end of body View
 }//end of Struct View
 
